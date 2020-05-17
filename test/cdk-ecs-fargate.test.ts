156ec8b657b6c469bbfadd9e10c04763fb2223bd
HeRoMo <hero@asterisk-works.jp>
2020-05-17 16:37:30 +0900

    add eslint

--- test/cdk-ecs-fargate.test.ts
+++ test/cdk-ecs-fargate.test.ts
@@ -3,11 +3,11 @@ import * as cdk from '@aws-cdk/core';
 import * as CdkEcsFargate from '../lib/cdk-ecs-fargate-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkEcsFargate.CdkEcsFargateStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkEcsFargate.CdkEcsFargateStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    Resources: {},
+  }, MatchStyle.EXACT));
 });
